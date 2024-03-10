ada9bb5f60394a886d68702198c5fe7785133a82
Haseeb Qureshi <haseeb.n.qureshi@gmail.com>
2024-03-10 08:02:54 -0400

    restructuring baseapi development to standalone

--- index.js
+++ index.js
@@ -1,29 +1,55 @@
 
 const path = require('path')
+const express = require('express')
+const fs = require('fs')
+const _ = require('underscore')
 
-const API = require('./_baseapi')({
-	envPath: path.resolve(__dirname, '.env'),
-	projectPath: path.resolve(__dirname),
-})
+module.exports = ({ projectPath, envPath }) => {
 
-// API.DB.students = {}
-// API.DB.students.readAll = async (where) => {
-// 	const result = await API.Utils.tryCatch(`try:students:readAll`,
-// 		API.DB.collection('user')
-// 			.find(where || {})
-// 			.toArray())
-// 	return result
-// }
+	require('dotenv').config({ path: envPath })
 
-// API.get('/students', async (req, res) => {
-// 	try {
-// 		const students = await API.Utils.tryCatch('try:students:readAll', 
-// 			API.DB.students.readAll({ role: 'student' }))
-// 		res.status(200).send({ data: students })
-// 	}
-// 	catch (err) {
-// 		API.Utils.errorHandler({ res, err })
-// 	}
-// })
+	const { 
+		name, 
+		auth,
+		routes, 
+		models, 
+		notifications 
+	} = require(path.resolve(projectPath, 'baseapi.config.js'))
 
-API.Start()
+	let API = express()
+	API.Express = express
+	API = require('./_utils')(API)
+	API = require('./_middlewares')(API) //must be the first thing, loads json middleware
+	API = require('./_checks')(API, { config: { projectPath }}) //before, so other features can load checks into the checker
+	API = require('./_notifications')(API, { config: notifications })
+	API = require('./_db')(API)
+	API = require('./_models')(API, { models })
+	API = require('./_auth')(API, { config: auth })
+	API = require('./_routes')(API, { routes: routes() })
+
+	API.Checks.enable()
+
+	API.Start = () => {
+		const port = process.env.PORT || 4000
+		API.listen(port, () => {
+			API.Log(`${name} BaseAPI started HTTP:${port} in ${process.env.NODE_ENV || 'development'} node environment ...`)
+		})
+	}
+
+	API.StartHTTPS = ({ key, crt }) => {
+		if (process.env.NODE_ENV !== 'production') {
+			return API.Log(`${name} BaseAPI attempted to start HTTPS in not-production node environment, quitting now ...`)
+		}
+		const httpServer = require('http').createServer(API)
+		const httpsServer = require('https').createServer({
+			key: fs.readFileSync(key, 'utf8'),
+			cert: fs.readFileSync(crt, 'utf8'),
+		}, API)
+		httpServer.listen(80)
+		httpsServer.listen(443)
+		API.Log(`${name} BaseAPI started HTTPS in production node environment ...`)
+	}
+
+	return API
+
+}
