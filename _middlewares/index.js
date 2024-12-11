7048834c4d63eab9f4221cfa231fd94a35db7682
Haseeb Qureshi <haseeb.n.qureshi@gmail.com>
2024-12-11 16:45:04 -0500

    increased body parser file limit

--- _middlewares/index.js
+++ _middlewares/index.js
@@ -8,8 +8,8 @@ module.exports = (API) => {
 	const morganMode = process.env.NODE_ENV === 'production' ? 'tiny' : 'dev'
 	API.use(morgan(morganMode))
 	API.use(cors())
-	API.use(bodyParser.urlencoded({ extended: false }))
-	API.use(bodyParser.json())
+	API.use(bodyParser.urlencoded({ limit: '5mb', extended: false }))
+	API.use(bodyParser.json({ limit: '5mb' }))
 
 	return API
 
